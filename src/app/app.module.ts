cc879416a95a2212fad5358353243d761a986b1a
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-01-14 17:16:01 -0500

    Cuarta app en Angular - spotiapp - Se configura las rutas de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
+import { APP_ROUTING } from './app.routes';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
